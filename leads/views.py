81e3988466c37d88bb23221caac26497448dcb6c
Aiden Schulman <aidenhschulman@gmail.com>
2023-09-15 17:49:58 -0400

    created individual lead details page

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Lead
@@ -9,3 +10,11 @@ def lead_list(request):
         "leads": leads
     }
     return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, "leads/lead_detail.html", context)
