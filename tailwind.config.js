646f8a26815f91b16a8bef23d08490663037e585
Aiden Schulman <aidenhschulman@gmail.com>
2023-09-18 11:38:49 -0400

    switched from tailwind cdn to node package

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./templates/**/*.html",
+        "./leads/templates/**/*.html",
+        "./agents/templates/**/*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
